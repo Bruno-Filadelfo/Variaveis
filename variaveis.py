af19ba2ff18f7a7c692ed85147abf1d63f8620dd
Bruno Filadelfo <bruno.filadelfo@outlook.com>
2023-08-24 05:16:08 -0300

    Variáveis

---
+++ variaveis.py
@@ -0,0 +1,84 @@
+#nome = "Bruno"
+#sobrenome = "Carvalho Filadelfo"
+#idade = 25
+#altura = 1.80
+#verdadeiro = True
+#profissao = "Desenvolvedor Python"
+#print('Nome Completo:', nome, sobrenome)
+#print('Idade:', idade)
+#print('Profissão:', profissao)
+#print('Altura:', altura)
+#print('Confirma essa Altura ?', True)
+
+
+#empresas_de_carro = "Toyota, Fiat, Chevrole"
+#modelos = "Corola, Touro, Cruze"
+#print(empresas_de_carro)
+#print(modelos)
+
+
+#idade = 27
+#altura = 1.88
+#numero = 15
+#print (type(idade))
+#print(type(altura))
+#print(type(numero))
+
+
+#largura = float(input("Digite a Largura do Retângulo: "))
+#altura = float(input("Digite a Altura do Retângulo: "))
+
+#area = largura * altura
+
+#print("Á area do Retângulo é:", area)
+
+#########################################
+
+#nome = input("Digite seu nome: ")
+#idade = int(input("Digite sua idade: "))
+
+#anos_faltantes = 100 - idade
+
+#ano_atual = 2023 
+#ano_100_anos = ano_atual + anos_faltantes
+
+#print(f"Olá, {nome}! Você terá 100 anos em {ano_100_anos}.")
+
+
+##########################################
+
+#matemática = int(input("Nota final de Matemática foi: "))
+#Português = int(input("Nota final de Portguês foi: "))
+#História = int(input("Nota final de História foi: "))
+
+
+#nota = matemática, Português, História
+
+#print(f"A, {nota}! a nota final foi")
+
+
+
+
+#nota1 = float(input("Digite a primeira nota: ")) 
+#nota2 = float(input("Digite a segunda nota: "))
+#nota3 = float(input("Digite a terceira nota: "))
+#media = (nota1 + nota2 + nota3) / 3
+#print("A média das notas é:", media)
+
+#numero = int(input("Digite um número inteiro: "))
+#dobro = numero * 2
+#print(f"O dobro de {numero} é {dobro}.")
+
+
+#ENTRADA DE DADOS 
+
+#nome = input("Qual é o seu nome? ")
+#idade = int(input("Quantos anos você tem ? "))
+#altura = float(input("Qual é a sua altura? "))
+#nascimento = input("Nascimento? ")
+#print(f"{nome} tem {idade} anos e {altura} de altura e nascido {nascimento}")
+
+numero1 = int(input("Digite um numero: "))
+numero2 = input("Digite outro numero: ")
+numero2 = int(numero2)
+print(numero1 / numero2)
