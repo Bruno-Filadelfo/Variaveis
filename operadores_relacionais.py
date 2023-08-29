065b2fa1a1c1c4c9ce59fe10c0eaae361fde9fda
Bruno Filadelfo <bruno.filadelfo@outlook.com>
2023-08-29 07:21:22 -0300

    Variáveis , Palavras-Chave , e Operadores Relacionais

--- operadores_relacionais.py
+++ operadores_relacionais.py
@@ -1,27 +1,27 @@
 #idades para criar um banco
-#nome = input("Qual é o seu nome? : ")
-#idade = input("Qual é a sua idade? : ")
-#idade = int(idade)
-#idade_maior = 17
-#idade_menor = 18
-#if idade >= idade_menor and idade <= idade_maior:
-#    print(f'{nome} Você não pode criar um banco por causa da sua.' f'{idade}')
-#else:
-#    print(f'{nome} Você pode criar um banco')   
+nome = input("Qual é o seu nome? : ")
+idade = input("Qual é a sua idade? : ")
+idade = int(idade)
+idade_maior = 17
+idade_menor = 18
+if idade >= idade_menor and idade <= idade_maior:
+    print(f'{nome} Você não pode criar um banco por causa da sua.' f'{idade}')
+else:
+    print(f'{nome} Você pode criar um banco')   
 
 
 #alistamento do quartel
 
-#idade = input("Tu tem quantos anos? : ")
-#nome = input("Seu nome? :")
-#idade = int(idade)
-#idade_menor = 18 
+idade = input("Tu tem quantos anos? : ")
+nome = input("Seu nome? : ")
+idade = int(idade)
+idade_menor = 18 
     
-#if idade >= idade_menor:
-#    print(f'{nome} tá com o limite certo Pode servir o quartel.')
-#else:
-#    idade <= idade_menor
-#    print(f'{nome} você não pode servir')   
+if idade >= idade_menor:
+    print(f'{nome} tá com o limite certo Pode servir o quartel.')
+else:
+    idade <= idade_menor
+    print(f'{nome} você não pode servir')   
 
 
 
