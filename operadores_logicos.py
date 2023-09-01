667e4b8a354804a3f483cc57af4db57baa446a4d
Bruno Filadelfo <bruno.filadelfo@outlook.com>
2023-09-01 07:59:51 -0300

    Variáveis , Palavras-Chave ,  Operadores Relacionais , Operadores Lógicos

--- operadores_logicos.py
+++ operadores_logicos.py
@@ -1,37 +1,37 @@
-#a = 2
-#b = 2
-#c = 3
-#resultado = a == b and c < b 
-#print(resultado)
+a = 2
+b = 2
+c = 3
+resultado = a == b and c < b 
+print(resultado)
 
-#a = 15 
-#b = 20 
-#c = 30
-#d = 35
-#resultado = a > d 
-#print(resultado)
-#print (type(resultado))
+a = 15 
+b = 20 
+c = 30
+d = 35
+resultado = a > d 
+print(resultado)
+print (type(resultado))
 
-#a = 10 
-#b = 10
-#c = 30
-#resultado = a == b and 30 > 10
-#print(resultado)
+a = 10 
+b = 10
+c = 30
+resultado = a == b and 30 > 10
+print(resultado)
 
-#a = 100
-#b = 200
-#c = 300
-#resultado = a == b  or 300 > 500
-#print(resultado)
+a = 100
+b = 200
+c = 300
+resultado = a == b  or 300 > 500
+print(resultado)
 
 
-#a = 1000
-#b = 2000
+a = 1000
+b = 2000
 
-#if not 2000 < 1000:
-#    print("B é maior que A")
-#else:
-#    print("A é maior que B")    
+if not 2000 < 1000:
+    print("B é maior que A")
+else:
+    print("A é maior que B")    
 
 
 a = 30 
