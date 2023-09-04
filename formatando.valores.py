daf4fcfc718e8a70877ac7f5559230445d7b6812
Bruno Filadelfo <bruno.filadelfo@outlook.com>
2023-09-04 02:53:18 -0300

    Variáveis , Palavras-Chave , Operadores Relacionais , Operadores Lógicos , Formatando Valores

---
+++ formatando.valores.py
@@ -0,0 +1,59 @@
+numero1 = 10
+numero2 = 20
+calculando = numero1 + numero2
+print(calculando)
+print(f'{calculando:.2f}')
+print('{:.2f}'.format(calculando))
+
+#######################
+
+numero1 = 10
+numero2 = 3
+calculando = numero1 / numero2
+print(calculando)
+print(f'{calculando:.2f}')
+print('{:.2f}'.format(calculando))
+
+#######################
+
+numero1 = 10
+numero2 = 3
+calculando = numero1 * numero2
+print(calculando)
+print(f'{calculando:.2f}')
+print('{:.2f}'.format(calculando))
+
+########################
+
+numero1 = 3
+numero2 = 10
+calculando = numero1 - numero2
+print(calculando)
+print(f'{calculando:.2f}')
+print('{:.2f}'.format(calculando))
+
+#########################
+
+nome = "Programado Júnior"
+print(nome.lower())
+print(nome.upper())
+print(nome.title())
+
+
+#########################
+
+numero1 = 1
+print(f'{numero1:0>10.2f}')
+numero2 = 1
+print(f'{numero2:0<10.2f}')
+
+numero1 = 5555
+print(f'{numero1:0>10.2f}')
+numero2 = 6666
+print(f'{numero2:0<10.2f}')
+
+numero1 = 5555 
+print(f'{numero1:0^10.2f}')
+numero2 = 8888
+print(f'{numero2:0^10.2f}')
+
