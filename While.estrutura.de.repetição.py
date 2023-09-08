a4f41358615c0dd290c5fab7a8ada48e567b5095
Bruno Filadelfo <bruno.filadelfo@outlook.com>
2023-09-08 05:25:08 -0300

    while estrutura de repetição

--- While.estrutura.de.repetição.py
+++ While.estrutura.de.repetição.py
@@ -1,3 +1,23 @@
-while True:
-    nome = input("Qual é seu nome ? ")
-    print(f'olá {nome}!')
+#while True: #loop infinito
+#    nome = input("Qual é seu nome? ")
+#    print(f'Olá {nome}!')
+
+################################
+
+#x = 0 
+#while x < 5:
+#    print(x)
+#    x = x + 1 
+#print('acabou')
+
+x = 0 
+while x <10:
+    y = 0
+
+    while y <10:
+        print(f'({x} , {y})')
+        y += 1
+
+    x += 1
+
+print('acabou')    
