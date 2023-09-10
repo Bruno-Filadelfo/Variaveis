dc0d9fc9356fd51d706d9b9862a705dc554e53ff
Bruno Filadelfo <bruno.filadelfo@outlook.com>
2023-09-10 04:56:06 -0300

    Iterando strings com while em Python

--- While.Else.Repetição.com.acumuladores.py
+++ While.Else.Repetição.com.acumuladores.py
@@ -1,11 +1,28 @@
-contador = 1
-acumulador = 1
-while contador <= 10:
-    print(contador , acumulador)
-    acumulador = acumulador + contador
-    contador +=1
-else:
-    print('Cheguei ao fim!')    
+#contador = 1
+#acumulador = 1
+#while contador <= 10:
+#    print(contador , acumulador)
+#    acumulador = acumulador + contador
+#    contador +=1
+#else:
+#   print('Cheguei ao fim!')    
 
 ###########################
+
+
+nome = "Programador"
+a = 0
+while a < 12:
+    print(nome [a])
+    print(a)
+    a += 2
+
+######################
+
+nome = "Programador Júnior"
+tamanho_string = len(nome)
+c = 0 
+while c < tamanho_string:
+    print(c, nome[c])
+    c += 1 
 
