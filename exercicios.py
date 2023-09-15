14cfd51550f11f14e33f7890b96c921f8a8cda84
Bruno Filadelfo <bruno.filadelfo@outlook.com>
2023-09-14 23:04:08 -0300

    exercícios

--- exercicios.py
+++ exercicios.py
@@ -1,8 +1,7 @@
-#Imprima uma mensagem que diga "Olá, [nome]! Você tem [idade] anos."
-nome = (input("Qual é o seu nome?: "))
-idade = int(input("Você tem quantos anos?: "))
-print(f'Olá {nome}, seja bem vindo!')
 
-nome = input("Qual é o seu nome?: ")
-idade = int(input("Qual é a sua idade?: "))  # Converter para um número inteiro
-print(f'Olá {nome}, seja bem-vindo! Você tem {idade} anos.')
+nome = input('Qual é o seu nome: ')
+idade = int (input('Qual é a sua idade: '))
+país = input('Mora em qual país:')
+estado = input('Você é de qual estado: ')
+cidade = input('Qual cidade: ')
+print('Seja bem vindo!')
