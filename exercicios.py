d0f86825cece070113b3c859db583dcfcab20853
Bruno Filadelfo <bruno.filadelfo@outlook.com>
2023-09-14 23:42:40 -0300

    exercícios

--- exercicios.py
+++ exercicios.py
@@ -1,7 +1,24 @@
 
-nome = input('Qual é o seu nome: ')
-idade = int (input('Qual é a sua idade: '))
-país = input('Mora em qual país:')
-estado = input('Você é de qual estado: ')
-cidade = input('Qual cidade: ')
-print('Seja bem vindo!')
+
+#Calculadora Simples
+
+num1 = int (input('Digite um numéro: '))
+operador = input('Digite o operador (+. -, *, /): ')
+num2 = int (input('Digite o segundo numéro: '))
+if operador == '+':
+    resultado = num1 + num2
+elif operador == '-':
+    resultado = num1 - num2
+elif operador == '*':
+    resultado = num1 * num2
+elif operador == '/':
+    resultado = num1 / num2
+    if num2 != 0:
+        resultado = num1 / num2
+    else:
+        resultado = 'Erro: Divisão por zero'
+else:
+    resultado = 'Operador inválido'
+print('resultado:',resultado)                    
+
+
