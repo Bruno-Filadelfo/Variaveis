d41f6c805a51c461176d82a0362b018b1642df91
Bruno Filadelfo <bruno.filadelfo@outlook.com>
2023-09-21 18:13:50 -0300

    listas

--- listas.py
+++ listas.py
@@ -4,12 +4,20 @@
 #string = 'A' , 'B' , 'C' , 'D' , 'E'
 #print(string[3])
 
-locomoção = ['Carro','moto']
-print(locomoção[0])
-print(locomoção)
-print(type(locomoção))
+#Listas
+#locomoção = ['Cro','moto']
+#print(locomoção[0])
+#print(locomoção)
+#print(type(locomoção))
 
-cores = 'Vermelho'
-print(cores[4])
-print(cores)
-print(type(cores))
+#cores = 'Vermelho'
+#print(cores[4])
+#print(cores)
+#print(type(cores))
+
+#numeros = 300
+#print(type(numeros))
+
+listas = ['amarelo' , 'verde', 'roxo' , 'preto' , 'laranja' , 'vermelho' , 'azul']
+print(listas[4:])
+
